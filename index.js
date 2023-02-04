d65b033f8318ae7fc7df35203d206cdd8a3fabfc
Icaro Luiz Albar Eduardo <icaro.albar@darede.com.br>
2023-02-04 12:53:00 -0300

    main route created

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const cors = require('cors')
 
 const app = express()
 
+const PrimaryRouters = require('./routers/PrimaryRouters')
+const UserRoutes = require('./routers/UserRoutes')
+const PetRoutes = require('./routers/PetRoutes')
+
 app.use(express.json())
 app.use(cors({ credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.static('public'))
 
-const UserRoutes = require('./routers/UserRoutes')
+app.use('/', PrimaryRouters)
 app.use('/users', UserRoutes)
-
-const PetRoutes = require('./routers/PetRoutes')
 app.use('/pets', PetRoutes)
 
 app.listen(5000, () => console.log('Servidor em funcionamento em http://localhost:5000'))
---
+++ routers/PrimaryRouters.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+
+router.get('/', (req,res) => res.send('Bem vindo ao sistema Pet & Pet do Icaro Albar'))
+
+
+module.exports = router
