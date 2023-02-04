f89f374f46d0aed788561dda8984e5b4309fd0c1
Icaro Luiz Albar Eduardo <icaro.albar@darede.com.br>
2023-02-04 10:52:42 -0300

    upload images

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -7,12 +7,15 @@ module.exports = class PetController {
 
   static async create(req, res) {
     const { name, age, weight, color } = req.body
+    const images = req.files
     const available = true
+
 
     if (!name) res.status(422).json({ message: "O nome é obrigatório" })
     if (!age) res.status(422).json({ message: "A idade é obrigatória" })
     if (!weight) res.status(422).json({ message: "O peso é obrigatório" })
     if (!color) res.status(422).json({ message: "A cor é obrigatória" })
+    if (images.length === 0) res.status(422).json({ message: "A imagem é obrigatória" })
 
     const token = getToken(req)
     const user = await getUserByToken(token)
@@ -31,6 +34,8 @@ module.exports = class PetController {
         phone: user.phone
       }
     })
+
+    images.map((image => pet.images.push(image.filename)))
 
     try {
       const newPet = await pet.save()
