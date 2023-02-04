dc0901b21ed7fb94188b4d2caa27a4d4af5439be
Icaro Luiz Albar Eduardo <icaro.albar@darede.com.br>
2023-02-04 17:07:57 -0300

    front-end finished

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -234,7 +234,32 @@ module.exports = class PetController {
 
     await Pet.findByIdAndUpdate(id, pet)
 
-    res.status(200).json({message: `A visita foi agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`})
+    res.status(200).json({ message: `A visita foi agendada com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}` })
+
+  }
+  static async concludeAdoption(req, res) {
+    const id = req.params.id
+
+    const pet = await Pet.findOne({ _id: id })
+
+    if (!pet) {
+      res.status(404).json({ message: "Pet não encontrado!" })
+      return
+    }
+
+    pet.available = false
+
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({ message: "Houve um problema, tente novamente mais tarde!" })
+      return
+    }
+
+    await Pet.findByIdAndUpdate(id, pet)
+
+    res.status(200).json({ message: "Parabéns! O ciclo de adoção foi finalizado com sucesso!" })
 
   }
 
